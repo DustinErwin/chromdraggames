13dec9419b86f9d578824f503b2e7a502e3d4950
Galahad <derwin499@gmail.com>
2022-03-06 18:41:00 -0500

    lots of updates -added toggle for hidden nav for smaller screens

--- client/src/pages/TableTopPage.js
+++ client/src/pages/TableTopPage.js
@@ -1,9 +1,13 @@
+import ContentContainer from "../components/ContentContainer/ContentContainer";
+import Footer from "../components/Footer/Footer";
 import NavBar from "../components/NavBar/NavBar";
 
 function TableTopPage() {
   return (
     <div>
       <NavBar></NavBar>
+      <ContentContainer></ContentContainer>
+      <Footer></Footer>
     </div>
   );
 }
