01cdb9331d45fb8a84530aa929abdcce4a39671e
Galahad <derwin499@gmail.com>
2022-04-05 14:46:30 -0400

    moved images

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -1,10 +1,20 @@
 import React from "react";
-
+import "react-google-photo/styles.css";
+import GooglePhoto from "react-google-photo";
 import "./ProductCard.css";
 
 //CardLeft has the image on the left
 export default function ProductCard(props) {
-  const pubUrl = "%PUBLIC_URL%";
+  const [open, setOpen] = React.useState(false);
+
+  const images = [
+    {
+      src: "https://images.unsplash.com/photo-1509420316987-d27b02f81864?dpr=1&auto=format&fit=crop&w=1500&q=80&cs=tinysrgb&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D",
+      width: 10,
+      height: 10,
+    },
+  ];
+
   return (
     <>
       <div className="prod-card">
